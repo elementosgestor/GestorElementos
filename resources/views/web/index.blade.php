aa9e5ce7ca00ded34a431ab7a7556f13385a250c
elementosgestor <daniel.garciag@autonoma.edu.co>
2018-02-25 14:51:12 -0500

    primercommit

---
+++ resources/views/web/index.blade.php
@@ -0,0 +1,206 @@
+@extends('layout.home')
+
+@section('content')
+<header>
+        <div class="container">
+          <div class="brand">
+            <h1 class="brand_name"><a href="./">Gestion</a></h1>
+            <p class="brand_slogan">Elementos</p>
+          </div><a href="callto:#" class=""></a>
+          </div>
+        <div id="stuck_container" class="stuck_container">
+          <div class="container">
+            <nav class="nav">
+              <ul data-type="navbar" class="sf-menu">
+                 <li><a href="index-1.html">Elementos</a>
+                  <ul>
+                    <li><a href="#">Registrar Elementos</a></li>
+                     <ul>
+                          </ul></a></li>
+                         </ul>
+                </li>
+                <li><a href="index-2.html">Categorias</a>
+                	<ul>
+                    <li><a href="#">Registrar Categorias</a></li>
+                     <ul>
+                          </ul></a></li>
+                         </ul>
+                
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </header>
+      <!--
+      ========================================================
+                                  CONTENT
+      ========================================================
+      -->
+      <main>
+        <section class="camera_container">
+          
+        
+       
+        <section class="well1">
+          <div class="container">
+            <div class="row">
+              <div class="grid_4">
+                <h2>Acerca</h2><img src="images/page-1_img01.jpg" alt="">
+                <p>En esta aplicacion se podra </p><a href="#" class="btn">Read more</a>
+              </div>
+              <div class="grid_4">
+                <h2>Services</h2>
+                <p>Gestion de elementos consumibles para el registro y .</p>
+                <ul class="marked-list">
+                  <li><a href="#">Lorem ipsum dolor sit amet </a></li>
+                  <li><a href="#">Conse ctetur adipisicing</a></li>
+                  <li><a href="#">Elit sed do eiusmod tempor</a></li>
+                  <li><a href="#">Incididunt ut labore</a></li>
+                  <li><a href="#">Et dolore magna aliqua</a></li>
+                  <li><a href="#">Ut enim ad minim veniam</a></li>
+                  <li><a href="#">Quis nostrud exercitation</a></li>
+                  <li><a href="#">Incididunt ut labore</a></li>
+                  <li><a href="#">Et dolore magna aliqua</a></li>
+                </ul><a href="#" class="btn">Read more</a>
+              </div>
+              <div class="grid_4">
+                <div class="info-box">
+                  <h2 class="fa-comment">Help center</h2>
+                  <hr>
+                  <h3>Ask professionals:</h3>
+                  <dl>
+                    <dt>Monday - Friday:</dt>
+                    <dd>8am-7pm</dd>
+                  </dl>
+                  <dl>
+                    <dt>Saturday:</dt>
+                    <dd>8am-5pm</dd>
+                  </dl>
+                  <dl>
+                    <dt>Sunday:</dt>
+                    <dd>1pm-5pm</dd>
+                  </dl>
+                  <hr>
+                  <h3>24/7 Online Support:</h3>
+                  <dl>
+                    <dt>800-2345-6789</dt>
+                  </dl>
+                </div>
+                <div class="owl-carousel">
+                  <div class="item">
+                    <blockquote class="box">
+                      <div class="box_aside"><img src="images/page-1_img02.jpg" alt=""></div>
+                      <div class="box_cnt__no-flow">
+                        <p>
+                          <q>Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</q>
+                        </p>
+                        <cite><a href="#">Incididunt ut labor</a></cite>
+                      </div>
+                    </blockquote>
+                  </div>
+                  <div class="item">
+                    <blockquote class="box">
+                      <div class="box_aside"><img src="images/page-1_img03.jpg" alt=""></div>
+                      <div class="box_cnt__no-flow">
+                        <p>
+                          <q>Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</q>
+                        </p>
+                        <cite><a href="#">Incididunt ut labor</a></cite>
+                      </div>
+                    </blockquote>
+                  </div>
+                  <div class="item">
+                    <blockquote class="box">
+                      <div class="box_aside"><img src="images/page-1_img04.jpg" alt=""></div>
+                      <div class="box_cnt__no-flow">
+                        <p>
+                          <q>Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</q>
+                        </p>
+                        <cite><a href="#">Incididunt ut labor</a></cite>
+                      </div>
+                    </blockquote>
+                  </div>
+                  <div class="item">
+                    <blockquote class="box">
+                      <div class="box_aside"><img src="images/page-1_img05.jpg" alt=""></div>
+                      <div class="box_cnt__no-flow">
+                        <p>
+                          <q>Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</q>
+                        </p>
+                        <cite><a href="#">Incididunt ut labor</a></cite>
+                      </div>
+                    </blockquote>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+      <!--
+      ========================================================
+                                  FOOTER
+      ========================================================
+      -->
+      <footer>
+        <section class="well3">
+          <div class="container">
+            <ul class="row contact-list">
+              <li class="grid_4">
+                <div class="box">
+                  <div class="box_aside">
+                    <div class="icon2 fa-map-marker"></div>
+                  </div>
+                  <div class="box_cnt__no-flow">
+                    <address>4578 Manizales,Caldas,Colombia<br/> </address>
+                  </div>
+                </div>
+                <div class="box">
+                  <div class="box_aside">
+                    <div class="icon2 fa-envelope"></div>
+                  </div>
+                  <div class="box_cnt__no-flow"><a href="mailto:#">info@demolink.org</a></div>
+                </div>
+              </li>
+              <li class="grid_4">
+                <div class="box">
+                  <div class="box_aside">
+                    <div class="icon2 fa-phone"></div>
+                  </div>
+                  <div class="box_cnt__no-flow"><a href="callto:#">800-2345-6789</a></div>
+                </div>
+                <div class="box">
+                  <div class="box_aside">
+                    <div class="icon2 fa-fax"></div>
+                  </div>
+                  <div class="box_cnt__no-flow"><a href="callto:#">800-2345-6790</a></div>
+                </div>
+              </li>
+              <li class="grid_4">
+                <div class="box">
+                  <div class="box_aside">
+                    <div class="icon2 fa-facebook"></div>
+                  </div>
+                  <div class="box_cnt__no-flow"><a href="#">Follow on facebook</a></div>
+                </div>
+                <div class="box">
+                  <div class="box_aside">
+                    <div class="icon2 fa-twitter"></div>
+                  </div>
+                  <div class="box_cnt__no-flow"><a href="#">Follow on Twitter</a></div>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </section>
+        <section>
+          <div class="container">
+            <div class="copyright">Business Company © <span id="copyright-year"></span>.&nbsp;&nbsp;<a href="index-5.html">Privacy Policy</a>More <a rel="nofollow" href="http://www.templatemonster.com/category.php?category=418&type=1" target="_blank">Business Templates at TemplateMonster.com</a>
+            </div>
+          </div>
+        </section>
+      </footer>
+    </div>
+    <script src="{{asset('js/script.js')}}"></script>
+
+@endsection
